2d2c4a0bfa42b2aa1f631bf7efbcd80bc5af51e2
fairlay <fairlay@protonmail.com>
2017-07-03 18:28:30 +0200

    CRICKET

--- Market.cs
+++ Market.cs
@@ -8,9 +8,56 @@ using System.Globalization;
 
 namespace FairlaySampleClient
 {
+    public static class CATEGORY
+    {
+        public const int SOCCER = 1;
+        public const int TENNIS = 2;
+        public const int GOLF = 3;
+        public const int OTHERSPORTS = 4;
+        public const int RUGBYUNION = 5;
+        public const int BOXING = 6;
+        public const int HORSERACING = 7;
+        public const int MOTORSPORT = 8;
+        public const int SPECIAL = 10;
+        public const int RUGBYLEAGUE = 11;
+        public const int BASKETBALL = 12;
+        public const int AMERICANFOOTBALL = 13;
+        public const int BASEBALL = 14;
+        public const int HOCKEY = 30;
+
+
+        public const int POLITICS = 15;
+        public const int FINANCIAL = 16;
+        public const int GREYHOUND = 17;
+        public const int VOLLEYBALL = 18;
+        public const int HANDBALL = 19;
+        public const int DARTS = 20;
+        public const int BANDY = 21;
+        public const int WINTERSPORTS = 22;
+        public const int BOWLS = 24;
+        public const int POOL = 25;
+        public const int SNOOKER = 26;
+        public const int TABLETENNIS = 27;
+        public const int CHESS = 28;
+        public const int FUN = 31;
+        public const int ESPORTS = 32;
+        public const int INPLAY = 33;
+        public const int RESERVED4 = 34;
+        public const int MIXEDMARTIALARTS = 35;
+        public const int RESERVED6 = 36;
+        public const int RESERVED = 37;
+        public const int CYCLING = 38;
+        public const int RESERVED9 = 39;
+        public const int BITCOIN = 40;
+        public const int BADMINTON = 42;
+
+
+
+    }
+
     public partial class MarketX :  IComparable<MarketX>
     {
-
+      
 
         public enum SettleType
         {
@@ -73,7 +120,7 @@ namespace FairlaySampleClient
 
         public string OrdBStr;
 
-
+        
         public string Comp { get; set; }
         public long ID;
        
