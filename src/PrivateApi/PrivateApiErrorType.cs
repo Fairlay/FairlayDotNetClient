ef3b5a4d53583fdd4116bf30e026c06456f32e6b
Hendrik <hendrik@0x00.to>
2017-08-21 17:13:19 +0200

    First simple private api request/response model

---
+++ src/PrivateApi/PrivateApiErrorType.cs
@@ -0,0 +1,12 @@
+﻿namespace FairlayDotNetClient.PrivateApi
+{
+	public enum PrivateApiErrorType
+	{
+		None,
+		General,
+		/// <summary>
+		/// There was an error in a subtask of a bulk change order reqeust
+		/// </summary>
+		SubtaskBulkChangeOrderReqeust
+	}
+}
---
+++ src/PrivateApi/PrivateApiResponseStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace FairlayDotNetClient.PrivateApi
+{
+	public class PrivateApiResponseStatus
+	{
+		public bool Succeeded { get; set; }
+		public PrivateApiErrorType ErrorType { get; set; }
+		public string ErrorMessage { get; set; }
+	}
+}
